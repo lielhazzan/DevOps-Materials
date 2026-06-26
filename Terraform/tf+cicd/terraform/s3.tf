6d2c44be3fb6e8f7326cebcb4bf3fe237413c5e8
liel hazzan <leal.hazzan@gmail.com>
2026-06-26 17:34:19 +0300

    Upload architecture diagram

--- Terraform/tf+cicd/terraform/s3.tf
+++ Terraform/tf+cicd/terraform/s3.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "frontend_bucket" {
-  bucket = "${var.project_name}-frontend-bucket"
+  bucket        = "${var.project_name}-frontend-bucket"
+  force_destroy = true
 }
 resource "aws_s3_bucket_public_access_block" "frontend_bucket_pab" {
   bucket = aws_s3_bucket.frontend_bucket.id
